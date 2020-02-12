647718561195a06e0a6b6d116a2e7dcc3ddf9f39
korstrack <korstrack.tereans@gmail.com>
2020-02-12 11:23:16 -0500

    sounds

--- Unity/First Contact/Assets/Scripts/AlienInteraction.cs
+++ Unity/First Contact/Assets/Scripts/AlienInteraction.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class AlienInteraction : MonoBehaviour
 {
 	public GameObject Alien;
+	public GameObject spawn;
 	private float grabTime = 4.0f;
 
     private IEnumerator OnTriggerEnter(Collider other)
@@ -15,6 +16,8 @@ public class AlienInteraction : MonoBehaviour
     		Alien.GetComponent<Animator>().SetTrigger("grab");
     		other.GetComponent<AudioSource>().Play();
     		yield return new WaitForSeconds(grabTime);
+    		Destroy(other);
+    		Instantiate(spawn, Alien.transform.GetChild(1).GetChild(0).GetChild(2).GetChild(0).GetChild(0).GetChild(0).GetChild(0).GetChild(0).GetChild(0).GetChild(0).transform.position, Quaternion.Euler(new Vector3(0,180,0)));
     		Alien.GetComponent<Animator>().SetTrigger("ReverseGrab");
     	}
     }
